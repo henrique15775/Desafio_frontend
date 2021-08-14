7afbef35d32c5a0fa2f16ffe0f6e4ce3d1a37d27
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2021-08-14 20:57:32 -0300

    segundo commit

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const bodyParser = require('body-parser');
 
 
 
-/*
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
-app.use(express.static('styles'));
-app.use(express.static('images'));
-app.set('view',__dirname +  'index.html');
+app.set('view',__dirname +  'public/index.html');
 
 
 app.get('/', (req,res) => {
-  res.sendFile('index.html');
+  res.render(view);
 })
-*/
+
 app.listen(8000);
+console.log('Escutando...');
