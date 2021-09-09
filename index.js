9774b228bc71644ce1ac1fe9bde90a7a89e53443
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2021-09-09 19:36:41 -0300

    commit preguiçoso

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 
 const express = require('express');
 const app = express();
-//const app = express.Router();
+
 
 const bodyParser = require('body-parser');
 const sequelado = require('sequelize');
 
 const Post = require('./javascript/Posts.js');
-//gvar http = require("http");
+
 const path = require('path');
-//const handlebars = require('express-handlebars');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname,'public')));
 app.set('oxi','./index.html');
-//app.set('view engine','handlebars')
-//app.engine('handlebars',handlebars({defaultLayout: 'main'}))
-//app.set('view',__dirname +  'public/index.html');
 
-app.set('linke','./sent.html');
+
 
 app.get('/',(req,res) => {
   return res.render(oxi);
-
 })
 app.post('/ola',(req,res) => {
   
@@ -39,13 +35,6 @@ app.post('/ola',(req,res) => {
   });
 
 });
-
-/*
-app.get('/mijo',(req,res) => {
-  res.render(oxi);
-});
-*/
-
 
 app.listen(8000);
 console.log('Escutando...');
